2c322670229d2044ea77436788071a6383c00602
chambacu1013 <chambacu1013@gmail.com>
2025-12-31 22:04:04 -0500

    Corrigiendo url del backend para la web

--- frontend-sisvillasol/src/services/api.js
+++ frontend-sisvillasol/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://backend-villasol.onrender.com/api",
 });
 
 // 1. INTERCEPTOR DE SALIDA (REQUEST)
