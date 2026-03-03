e3fcb2c3e07b84b704e7a4482cebb98b1681c1e0
chambacu1013 <chambacu1013@gmail.com>
2026-03-02 23:11:24 -0500

    si le bajo a 73 r que pasa?

--- frontend-sisvillasol/src/pages/MapaFinca.jsx
+++ frontend-sisvillasol/src/pages/MapaFinca.jsx
@@ -60,7 +60,7 @@ const MapaFinca = () => {
     const detalleRef = useRef(null);
 
     // Coordenadas centrales
-    const centroFinca = [7.1472, -72.6690]; 
+    const centroFinca = [7.1473, -72.6690]; 
 
     useEffect(() => {
         const cargarLotes = async () => {
